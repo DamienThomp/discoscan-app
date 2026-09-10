9db80a7793391076539cb82bab4237b30b2f1483
Damien Thompson <sedativo@gmail.com>
2026-09-10 11:50:11 -0400

    Initial Commit

---
+++ DiscoScan/DiscoScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscoScanApp.swift
+//  DiscoScan
+//
+//  Created by Damien L Thompson on 2026-09-10.
+//
+
+import SwiftUI
+
+@main
+struct DiscoScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DiscoScanTests/DiscoScanTests.swift
@@ -0,0 +1,19 @@
+//
+//  DiscoScanTests.swift
+//  DiscoScanTests
+//
+//  Created by Damien L Thompson on 2026-09-10.
+//
+
+import Testing
+@testable import DiscoScan
+
+struct DiscoScanTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
